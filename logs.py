a47fdae76678094d5db0d9f35bda84bda20745b9
Dennis <dhilhorst2000@gmail.com>
2024-07-10 10:10:37 +0200

    specify level in log processor logging

--- logs.py
+++ logs.py
@@ -180,6 +180,7 @@ def tail_log_file():
 
 if __name__ == '__main__':
     logging.basicConfig(
+        level=logging.INFO,
         format="[%(asctime)s %(name)s:%(levelname)s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
